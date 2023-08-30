dd148f9a6c7fcfdd2eed3167fd5e6adf02b9b74e
Adam Corren <adamcorren@outlook.com>
2023-08-30 17:04:12 +0100

    Add files via upload

---
+++ lib/calculations.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+# calculate percentage change between two columns
+def calculate_pct_change(data, numerator_col, denominator_col, col_name):
+    return data.assign(**{col_name: lambda x: x[[numerator_col, denominator_col]].pct_change(axis=1)[denominator_col]})
+
+
+# calculate percentage change between two columns and change column name based on time argument 
+def calculate_pct_change_time(data, numerator_col, denominator_col, col_name, time):
+    return (data
+        .assign(**{col_name: lambda x: x[[numerator_col, denominator_col]].pct_change(axis=1)[denominator_col]})
+        .rename(columns={col_name: f'{col_name}_{time}'}))
+
+
+# rank metric comapred to other runners in race
+def rank_by_race_ascending(data, col_name, group, metric):
+    return data.assign(**{col_name: lambda x: x.groupby([group])[metric].rank(ascending=True, method='min')})
+
+
+# get mean value in group
+def mean_value(data, col_name, group, metric):
+    return data.assign(**{col_name: lambda x: x.groupby([group])[metric].transform('mean')})
+
+
+# rank metric comapred to other runners in race and change column name based on time argument 
+def rank_by_race_ascending_time(data, col_name, group, metric, time):
+    return (data
+        .assign(**{col_name: lambda x: x.groupby([group])[metric].rank(ascending=True, method='min')})
+        .rename(columns={col_name: f'{col_name}_{time}'}))
+
+
+# rank metric comapred to other runners in race descending
+def rank_by_race_descending(data, col_name, group, metric):
+    return data.assign(**{col_name: lambda x: x.groupby([group])[metric].rank(ascending=False, method='min')})
+
+
+# rank metric comapred to other runners in race descending and change column name based on time argument 
+def rank_by_race_descending_time(data, col_name, group, metric, time):
+    return (data
+        .assign(**{col_name: lambda x: x.groupby([group])[metric].rank(ascending=False, method='min')})
+        .rename(columns={col_name: f'{col_name}_{time}'}))
+
+
+# get favourite based on current price
+def get_favourite(data, col_name, metric, time):
+    return (data
+            .assign(**{col_name:0})
+            .assign(**{col_name:lambda data : data[col_name].where(data[metric] != 1, 1)})
+            .rename(columns={col_name: f'{col_name}_{time}'}))
+
---
+++ lib/non_runner_adjustments.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+# getting all the names of the days non runners
+def get_non_runners(race_data):
+    return (race_data['non_runners']
+            .drop_duplicates()
+            .dropna()
+            .apply(lambda x: eval(x))
+            .explode()
+            .str.strip()
+            .reset_index(drop=True))
+    
+
+# getting last price of non runners (if price over 15 no deductions apply)
+def get_non_runners_price(race_data, hourly_odds):
+    return (pd.merge(race_data, hourly_odds, left_on='non_runners', right_on='name', how='left')
+            .groupby('non_runners',as_index=False)
+            .agg({'time': 'first', 'location': 'first', 'non_runners': 'first',
+                  'logged': 'last', 'spr': 'last'})
+            .astype({'spr': 'float16'})
+            .dropna()
+            .query('spr < 15')
+            .reset_index(drop=True))
+
+    
+# getting deductions to remaining runners based on last price of non runner
+def get_non_runners_deductions(race_data):
+    prices, deductions = [
+        [0.00, 1.11, 1.18, 1.25, 1.30, 1.40, 1.53, 1.62, 1.80, 1.95,
+         2.20, 2.50, 2.75, 3.39, 4.00, 5.00, 6.50, 10.00, 14.99, 1000],
+                          [0.90, 0.85, 0.80, 0.75, 0.70, 0.65, 0.60, 0.55, 0.50, 0.45,
+                           0.40, 0.35, 0.30, 0.25, 0.20, 0.15, 0.10, 0.05, 0.00]]
+    return (race_data
+            .assign(deduction=pd.cut(race_data.spr, bins=prices, labels=deductions).astype('float'))
+            .groupby(['time', 'logged'], as_index=False)
+            .agg({'time': 'first', 'logged': 'first', 'deduction': 'sum'})
+            .loc[:, ['time', 'logged', 'deduction']])
+    
+
+def get_non_runner_adjustments(race_data, hourly_odds):
+    print('Non runner price adjustments complete...')
+    return (get_non_runners(race_data)
+            .pipe(get_non_runners_price, hourly_odds)
+            .pipe(get_non_runners_deductions))
+
+
